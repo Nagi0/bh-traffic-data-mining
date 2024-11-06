dfe06f2d2f37bed051962f9401f085a30c995216
Nagi0 <brunolefonseca13@gmail.com>
2024-11-05 23:33:46 -0300

    feat: add data loader

--- bhTrafficDataMining/dataProcessing/main.py
+++ bhTrafficDataMining/dataProcessing/main.py
@@ -1,26 +1,12 @@
 import os
-from glob import glob
-from tqdm import tqdm
 from dotenv import load_dotenv
-import polars as pl
+from dataLoader import DataLoader
 
 
 if __name__ == "__main__":
-    load_dotenv("./config/.env")
-    json_files = glob("./data/jan/*.json")
-    day_folders = os.listdir("./data/jan/")
-
-    dataframes_list = []
-    for folder in tqdm(day_folders):
-        json_files = glob(f"./data/jan/{folder}/*.json")
-        for file in tqdm(json_files):
-            df = pl.read_json(file)
-            filtered_df = (
-                df.lazy()
-                .filter(pl.col("ENDEREÇO").str.contains(f"{os.environ["TARGET"]}"))
-                .collect()
-            )
-            dataframes_list.append(filtered_df)
-
-    month_df = pl.concat(dataframes_list)
-    print(month_df)
+    load_dotenv("bhTrafficDataMining/dataProcessing/config/.env")
+    data_loader = DataLoader(
+        "bhTrafficDataMining/dataProcessing/data/", os.environ["TARGET"]
+    )
+    df = data_loader.load_data()
+    print(df)
