056d0a10f38dec9d85d7808e5a27f8cd665560f1
Nagi0 <brunolefonseca13@gmail.com>
2024-11-17 14:48:25 -0300

    feat: add humam attempt

--- bhTrafficDataMining/humanAttempt/dataProcessing/dataLoader.py
+++ bhTrafficDataMining/humanAttempt/dataProcessing/dataLoader.py
@@ -15,28 +15,92 @@ class DataLoader:
         json_files_list = []
 
         for m_folder in months_folders:
-            d_folder_list = os.listdir(f"{self.dir}/{m_folder}")
-            for d_folder in d_folder_list:
-                json_files = glob(f"{self.dir}/{m_folder}/{d_folder}/*.json")
-                json_files_list.extend(json_files)
+            m_folder = os.path.join(self.dir, m_folder)
+            if os.path.isdir(m_folder):
+                d_folder_list = os.listdir(m_folder)
+                for d_folder in d_folder_list:
+                    d_folder = os.path.join(m_folder, d_folder)
+                    json_files = glob(f"{d_folder}/*.json")
+                    json_files_list.extend(json_files)
 
         return json_files_list
+
+    def filter_location(self, p_df: pl.DataFrame):
+        filtered_df = (
+            p_df.lazy()
+            .filter(pl.col("ENDEREÇO").str.contains(f"{self.target}"))
+            .collect()
+        )
+
+        return filtered_df
+
+    def check_above_speed_limite(self, p_df: pl.DataFrame):
+        df = p_df.with_columns(
+            pl.col("VELOCIDADE AFERIDA").cast(pl.Float32),
+            pl.col("VELOCIDADE DA VIA").cast(pl.Float32),
+        )
+        df = df.with_columns(
+            (pl.col("VELOCIDADE AFERIDA") > pl.col("VELOCIDADE DA VIA")).alias(
+                "ULTRAPASSOU LIMITE"
+            )
+        )
+
+        return df
+
+    def categorize_time_of_day_extended(self, hour):
+        if 0 <= hour < 6:
+            return "Madrugada"
+        elif 6 <= hour < 9 or 17 <= hour < 20:
+            return "Pico"
+        elif 9 <= hour < 12:
+            return "Manhã"
+        elif 12 <= hour < 17:
+            return "Tarde"
+        else:
+            return "Noite"
+
+    def discretize_datetime(self, p_df: pl.DataFrame):
+        df = p_df.with_columns(
+            pl.col("DATA HORA").str.strptime(pl.Datetime, format="%Y-%m-%dT%H:%M:%S")
+        )
+
+        df = (
+            df.lazy()
+            .with_columns(
+                pl.col("DATA HORA")
+                .dt.hour()
+                .map_elements(
+                    self.categorize_time_of_day_extended, return_dtype=pl.String
+                )
+                .alias("PERIODO DIA")
+            )
+            .collect()
+        )
+
+        return df
 
     def load_data(self) -> pl.DataFrame:
         json_files_list = self.list_files()
         dataframes_list = []
+
         for file in tqdm(json_files_list):
             df = pl.read_json(file)
-            filtered_df = (
-                df.lazy()
-                .filter(pl.col("ENDEREÇO").str.contains(f"{self.target}"))
-                .collect()
-            )
-            filtered_df = filtered_df.with_columns(
-                (pl.col("VELOCIDADE AFERIDA") > pl.col("VELOCIDADE DA VIA")).alias(
-                    "ULTRAPASSOU LIMITE"
-                )
-            )
+            filtered_df = self.filter_location(df)
+            filtered_df = self.check_above_speed_limite(filtered_df)
+
+            filtered_df = self.discretize_datetime(filtered_df)
+
+            filtered_df = filtered_df[
+                "PERIODO DIA",
+                "ENDEREÇO",
+                "SENTIDO",
+                "FAIXA",
+                "CLASSIFICAÇÃO",
+                "TAMANHO",
+                "VELOCIDADE DA VIA",
+                "VELOCIDADE AFERIDA",
+                "ULTRAPASSOU LIMITE",
+            ]
             dataframes_list.append(filtered_df)
 
         months_df = pl.concat(dataframes_list)
---
+++ bhTrafficDataMining/humanAttempt/patternMining/preprocessor.py
@@ -0,0 +1,67 @@
+import os
+from glob import glob
+from dataclasses import dataclass
+from tqdm import tqdm
+import polars as pl
+import pandas as pd
+
+
+@dataclass
+class DataPreprocessor:
+    dir: str
+
+    def load_database(self, p_percentage) -> pl.DataFrame:
+        csv_files_list = glob(f"{self.dir}/*.csv")
+        percent_index = int(round(len(csv_files_list) * p_percentage))
+        dataframes_list = []
+        for file in tqdm(csv_files_list[:percent_index]):
+            df = pl.read_csv(file)
+            df = self.discretize_speed(df)
+            dataframes_list.append(df)
+
+        return pl.concat(dataframes_list)
+
+    def get_speep_intervals(self, p_speed):
+        if 0 <= p_speed < 50:
+            return "velocidadeModerada"
+        elif 50 <= p_speed < 100:
+            return "velocidadeAlta"
+        elif p_speed >= 100:
+            return "velocidadeAltissima"
+
+    def discretize_speed(self, p_df: pl.DataFrame):
+        df = (
+            p_df.lazy()
+            .with_columns(
+                pl.col("VELOCIDADE AFERIDA")
+                .map_elements(self.get_speep_intervals, return_dtype=pl.String)
+                .alias("VELCOIDADE")
+            )
+            .collect()
+        )
+
+        return df
+
+    def drop_undesired_columns(
+        self, p_df: pl.DataFrame, p_undesired_columns: list
+    ) -> pl.DataFrame:
+        return p_df.drop(p_undesired_columns)
+
+    def get_preprocessed_database(self, p_percentage: float, p_undesired_columns: list):
+        df = self.load_database(p_percentage)
+        df = self.drop_undesired_columns(df, p_undesired_columns)
+        df = df.to_dummies()
+        df = df.cast(pl.Boolean)
+
+        return df
+
+
+if __name__ == "__main__":
+    data_preprocessor = DataPreprocessor(
+        "bhTrafficDataMining/humanAttempt/dataProcessing/dataProcessed"
+    )
+
+    df = data_preprocessor.get_preprocessed_database(
+        0.5, p_undesired_columns=["VELOCIDADE AFERIDA", "VELOCIDADE DA VIA", "TAMANHO"]
+    )
+    print(df)
