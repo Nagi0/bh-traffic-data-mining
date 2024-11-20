a5cd3aa71c0e52c352fef9173711a2138efd0bf6
Nagi0 <brunolefonseca13@gmail.com>
2024-11-20 18:22:57 -0300

    feat: add phase3 initial commit

--- bhTrafficDataMining/humanAttempt/patternMining/main.py
+++ bhTrafficDataMining/humanAttempt/patternMining/main.py
@@ -1,5 +1,5 @@
 import polars as pl
-from mlxtend.frequent_patterns import fpgrowth
+from mlxtend.frequent_patterns import fpgrowth, association_rules
 from preprocessor import DataPreprocessor
 
 
@@ -12,9 +12,19 @@ if __name__ == "__main__":
         1.0, p_undesired_columns=["VELOCIDADE AFERIDA", "VELOCIDADE DA VIA", "TAMANHO"]
     )
     df = df.lazy().filter(pl.col("ULTRAPASSOU LIMITE_true") == True).collect()
+    df = df.drop(["ULTRAPASSOU LIMITE_true", "ULTRAPASSOU LIMITE_false"])
     df = df.to_pandas()
     print(df)
 
     frequent_itemsets = fpgrowth(df, min_support=0.25, use_colnames=True, verbose=1)
     print(frequent_itemsets)
     frequent_itemsets.to_csv("bhTrafficDataMining/humanAttempt/resultados.csv")
+
+    rules = association_rules(
+        frequent_itemsets,
+        metric="confidence",
+        min_threshold=0.75,
+        num_itemsets=df.shape[0],
+    )
+    print(rules)
+    rules.to_csv("bhTrafficDataMining/humanAttempt/association_rules_conf75.csv")
